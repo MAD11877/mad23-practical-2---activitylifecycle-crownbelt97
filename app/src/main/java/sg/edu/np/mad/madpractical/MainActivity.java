63afa562bef35ab968d94cbdce819be66cbf00b6
crownbelt97 <blaziken_jeremychan@live.com.sg>
2023-05-02 22:32:58 +0800

    Week 2 Practical

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -3,12 +3,25 @@ package sg.edu.np.mad.madpractical;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean following = false;
+    private Button followBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        followBtn = (Button) findViewById(R.id.followBtn);
+        followBtn.setOnClickListener(view -> {
+            if(!following){
+                followBtn.setText("Unfollow");
+            }
+            else{
+                followBtn.setText("follow");
+            }
+            following = !following;
+        });
     }
 }
